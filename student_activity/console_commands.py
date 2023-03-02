5cf22d4ead2cd85957ee5c8f39a2917ef24e0d2d
dalesa <andrew.dales@highgateschool.org.uk>
2023-03-02 15:04:29 +0000

    Added .gitignore. Changes to console_commands.py in library

--- student_activity/console_commands.py
+++ student_activity/console_commands.py
@@ -5,8 +5,8 @@ from models import Person, Activity, Location
 # Connect to the activities database
 engine = create_engine('sqlite:///activities.sqlite', echo=True)
 
-# Create a session and add the people to the database
-with Session(engine) as sess:
-    persons = sess.query(Person).all()
-    activities = sess.query(Activity).all()
-    locations = sess.query(Location).all()
+# # Create a session and add the people to the database
+# with Session(engine) as sess:
+#     persons = sess.query(Person).all()
+#     activities = sess.query(Activity).all()
+#     locations = sess.query(Location).all()
